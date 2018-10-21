9e7b19792322aa2c1e10bc230ade52aa1364a6e0
milenaliz <mlizurej@o2.pl>
2018-10-21 15:22:56 +0200

    first commit

---
+++ src/com/sda/sort/Main.java
@@ -0,0 +1,15 @@
+package com.sda.sort;
+//Tworzymy projekt -BubbleSort - com.sda.sort,
+//Tworzymy plik .gitignore(.idea *.iml out),
+//Tworzymy repozytorium na github BubbleSort_XX,
+//Tworzymy repozytorium lokalne
+//git init
+//git add .class
+// git commit -m ""
+//
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
